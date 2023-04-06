a75415ac363de9273ede8ea9021406a81b519541
Hazeem Hassan <hhazeemhassan@gmail.com>
2023-04-06 10:51:17 +0500

    Initial commit

---
+++ src/main/java/springframeworkmicroservices/kbesbmicroservices/KbeSbMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package springframeworkmicroservices.kbesbmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeSbMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbeSbMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springframeworkmicroservices/kbesbmicroservices/KbeSbMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package springframeworkmicroservices.kbesbmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeSbMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
